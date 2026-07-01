5a9767572a0bc6270bb651e43a767238d8b51850
Shiva Nath Roy <shivanathroy@gmail.com>
2026-07-01 22:20:36 +0530

    fix: full OIDC terraform pipeline

--- terraform-project/providers.tf
+++ terraform-project/providers.tf
@@ -1,26 +1,6 @@
-terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.47"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-  use_cli = false
 }
 
 provider "azuread" {
-  tenant_id = var.tenant_id
-  use_cli   = false
 }
