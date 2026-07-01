0a48a4b8e33042731c2d01f7674a4974153216ae
Shiva Nath Roy <shivanathroy@gmail.com>
2026-07-01 23:30:41 +0530

    chore: migrate architecture to secure azure remote backend

--- terraform-project/providers.tf
+++ terraform-project/providers.tf
@@ -13,6 +13,16 @@ terraform {
       version = "~> 3.5"
     }
   }
+
+  # THIS CONTEXT SYNCS MACBOOK AND GITHUB TOGETHER
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "shivastfstatestorage" # Ensure this matches step 1 exactly
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
