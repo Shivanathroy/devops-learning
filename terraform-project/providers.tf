acbca7e1cf4b85308ea96726feb3f439fd971b63
Shiva Nath Roy <shivanathroy@gmail.com>
2026-07-01 22:23:54 +0530

    updated

--- terraform-project/providers.tf
+++ terraform-project/providers.tf
@@ -1,6 +1,25 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
 
 provider "azuread" {
+  use_oidc = true
 }
